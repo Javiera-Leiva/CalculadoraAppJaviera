9ed8715f50285cf8b00ad454735298fa190fcce0
Javiera-Leiva <javieraleiva026@gmail.com>
2023-06-11 10:07:43 -0600

    inicializaciòn de la const math y creacion de estructura

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,3 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { create, all } from 'mathjs';
+
+const math = create(all);
+
+const Calculadora = () => {
+    
+}
