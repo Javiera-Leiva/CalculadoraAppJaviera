5b518620dc3f70368ea93b504000da8063d8d606
Javiera-Leiva <javieraleiva026@gmail.com>
2023-06-11 10:12:15 -0600

    Creación de la variable de estado y función de validación de botones

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -5,5 +5,9 @@ import { create, all } from 'mathjs';
 const math = create(all);
 
 const Calculadora = () => {
-    
+    const [resultado, setResultado] = useState('');
+
+    const handleButtonPress = (value) => {
+      setResultado(resultado + value);
+    };
 }
